35471ebaa50fa24a2018e24f6ac5419d609f9f2f
Felix Brunnberg <h23felbr@du.se>
2025-06-05 11:49:02 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab7BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

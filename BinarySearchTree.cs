d15329e54d194ce7f8eaee8bff6cf963f6d3fc3d
Felix Brunnberg <h23felbr@du.se>
2025-06-05 14:49:24 +0200

    Lade till Add och Search funktionalitet

--- BinarySearchTree.cs
+++ BinarySearchTree.cs
@@ -8,31 +8,101 @@ namespace Lab7BinarySearchTree
 {
     internal class BinarySearchTree
     {
-        TreeNode root;
+        public TreeNode root;
 
-        public BinarySearchTree(int value) 
+        public BinarySearchTree(int value)
         {
             root = new TreeNode(value);
         }
 
         public void AddIterative(int value)
         {
-
+            TreeNode current = root;
+            bool loop = true;
+            while (loop)
+            {
+                if (value == current.Value)
+                {
+                    throw new Exception("Cannot add duplicate key");
+                }
+                else if (value < current.Value)
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = new TreeNode(value);
+                        loop = false;
+                    }
+                    current = current.Left;
+                }
+                else
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = new TreeNode(value);
+                        loop = false;
+                    }
+                    current = current.Right;
+                }
+            }
         }
 
         public void AddRecursive(int value)
         {
-
+            if (value == root.Value)
+            {
+                throw new Exception("Cannot add duplicate key");
+            }
+            AddRecursiveHelper(root, value);
         }
 
         private void AddRecursiveHelper(TreeNode current, int value)
         {
+            if (value < current.Value)
+            {
+                if (current.Left == null)
+                {
+                    current.Left = new TreeNode(value);
+                    return;
+                }
+                AddRecursiveHelper(current.Left, value);
+            }
+            else
+            {
+                if (current.Right == null)
+                {
+                    current.Right = new TreeNode(value);
+                    return;
+                    
+                }
+                AddRecursiveHelper(current.Right, value);
+            }
 
         }
 
         public void DeleteIterative(int value)
         {
+            TreeNode current = root;
+            TreeNode prev = current;
 
+            while (current != null && current.Value != value)
+            {
+                if (value < current.Value)
+                {
+                    prev = current;
+                    current = current.Left;
+                }
+                else
+                {
+                    prev = current;
+                    current = current.Right;
+                }
+            }
+            if (current != null)
+            {
+                throw new Exception("Value does not exist");
+            }
+
+            
         }
 
         public void DeleteRecursive(int value)
@@ -40,23 +110,55 @@ namespace Lab7BinarySearchTree
 
         }
 
-        private void DeleteRecursiveHelper(TreeNode current, int value) 
-        { 
-        
-        }
-
-        public void SearchIterative(int value)
+        private void DeleteRecursiveHelper(TreeNode current, int value)
         {
 
         }
 
-        public void SearchRecursiveint(int value)
+        public bool SearchIterative(int value)
         {
+            TreeNode current = root;
 
+            while (current != null && current.Value != value)
+            {
+                if (value < current.Value)
+                {
+                    current = current.Left;
+                }
+                else
+                { 
+                    current = current.Right;
+                }
+            }
+            if (current == null)
+                return false;
+            return true;
         }
 
-        private void SearchRecursiveHelper(TreeNode current, int value)
+        public bool SearchRecursive(int value)
         {
+            return SearchRecursiveHelper(root, value);
+        }
+
+        private bool SearchRecursiveHelper(TreeNode current, int value)
+        {
+            if (current == null)
+            {
+                return false;
+            }
+            if (value == current.Value)
+            {
+                return true;
+            }
+            else if (value < current.Value)
+            { 
+                return SearchRecursiveHelper(current.Left, value);
+            }
+            else
+            {
+                return SearchRecursiveHelper(current.Right, value);
+            }
+
 
         }
     }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,33 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            BinarySearchTree bst = new BinarySearchTree(20);
+            bst.AddRecursive(19);
+            bst.AddRecursive(21);
+            bst.AddRecursive(22);
+            Console.WriteLine(bst.root.Left);
+            Console.WriteLine(bst.root.Right);
+            Console.WriteLine(bst.root.Right.Right);
+            Console.WriteLine();
+
+            Console.WriteLine(bst.SearchIterative(22));
+            Console.WriteLine(bst.SearchRecursive(22));
+            try
+            {
+                Console.WriteLine(bst.SearchIterative(23));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                Console.WriteLine(bst.SearchRecursive(23));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }

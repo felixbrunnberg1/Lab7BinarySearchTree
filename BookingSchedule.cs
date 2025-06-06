6cdd16b4c5dc7e9679e413a5fffe5a7eddcec0d7
Isak Stöt <h23issto@du.se>
2025-06-06 14:06:58 +0200

    la till event och bookingschedule klass

---
+++ BookingSchedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7BinarySearchTree
+{
+    internal class BookingSchedule
+    {
+        private BinarySearchTree schedule;
+        FindNextAvailableEvent(DateTime startTime);
+    }
+}

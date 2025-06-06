087e506f291be86dc7ee3a6af14df5a13a6e3ac6
Isak Stöt <h23issto@du.se>
2025-06-06 14:55:08 +0200

    merge

--- BookingSchedule.cs
+++ BookingSchedule.cs
@@ -9,6 +9,32 @@ namespace Lab7BinarySearchTree
     internal class BookingSchedule
     {
         private BinarySearchTree schedule;
-        FindNextAvailableEvent(DateTime startTime);
+        private Dictionary<DateTime, Event> events;
+
+
+        public BookingSchedule()
+        {
+            schedule = new BinarySearchTree();
+        }
+        public Event FindNextAvailableEvent(DateTime startTime)
+        {
+
+        }
+
+        public void AddEvent(Event newEvent)
+        {
+            if (events.ContainsKey(newEvent.EventDate))
+            {
+                throw new Exception("Event already exists on this date.");
+            }
+
+            events.Add(newEvent.EventDate, newEvent);
+            schedule.AddIterative((int)newEvent.EventDate.Ticks);
+        }
+
+        public Event[] ShowEventsAfter(DateTime startTime)
+        {
+
+        }
     }
 }

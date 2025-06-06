4c3abd22383e6ffbc295f0e7e1c14276b753ba7c
Isak Stöt <h23issto@du.se>
2025-06-06 15:11:13 +0200

    la till showeventsafter

--- BookingSchedule.cs
+++ BookingSchedule.cs
@@ -10,32 +10,45 @@ namespace Lab7BinarySearchTree
     {
         private BinarySearchTree schedule;
 
-        private Dictionary<DateTime, Event> events;
+        private Dictionary<int, Event> events;
 
 
         public BookingSchedule()
         {
             schedule = new BinarySearchTree();
         }
+
         public Event FindNextAvailableEvent(DateTime startTime)
         {
-
+            int startTimeInt = (int)startTime.Ticks;
+            TreeNode nextEvent = schedule.GetNextInorder(startTimeInt);
+            return events[nextEvent.Value];
         }
 
         public void AddEvent(Event newEvent)
         {
-            if (events.ContainsKey(newEvent.EventDate))
+            if (events.ContainsKey((int)newEvent.EventDate.Ticks))
             {
                 throw new Exception("Event already exists on this date.");
             }
 
-            events.Add(newEvent.EventDate, newEvent);
+            events.Add((int)newEvent.EventDate.Ticks, newEvent);
             schedule.AddIterative((int)newEvent.EventDate.Ticks);
         }
 
-        public Event[] ShowEventsAfter(DateTime startTime)
+        public void ShowEventsAfter(DateTime startTime)
         {
-
+            while(schedule.root != null)
+            {
+                Event current = FindNextAvailableEvent(startTime);
+                if (current == null)
+                {
+                    break;
+                }
+                Event ev = events[(int)current.EventDate.Ticks];
+                Console.WriteLine($"Event: {ev.EventName}, Date: {ev.EventDate}");
+                startTime = ev.EventDate;
+            }
         }
 
     }
